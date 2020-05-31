b0aca9cc4dcef04904ff8c6655ff34c7be3ca3d1
Aaron Castro <aaroncrcr@gmail.com>
2020-05-31 16:22:35 -0300

    admin panel start

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,7 +6,7 @@ import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { CustomersTableComponent } from './customers-table/customers-table.component';
 import { InvoicesTableComponent } from './invoices-table/invoices-table.component';
 import { EmployeesTableComponent } from './employees-table/employees-table.component';
-import { MaterialModule } from '../shared/material.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -19,7 +19,7 @@ import { MaterialModule } from '../shared/material.module';
   imports: [
     CommonModule,
     AdminRoutingModule,
-    MaterialModule
+    SharedModule
   ]
 })
 export class AdminModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DialogComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   exports: [
     ReactiveFormsModule,
-    MaterialModule
+    MaterialModule,
+    DialogComponent,
   ]
 })
 export class SharedModule { }
