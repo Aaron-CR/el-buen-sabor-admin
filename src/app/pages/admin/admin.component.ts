44ebdc2f81b8976902a6ed4f858892e097738a89
Aaron Castro <aaroncrcr@gmail.com>
2020-06-07 18:01:13 -0300

    admin charts added

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,80 +1,240 @@
 import { Component, OnInit } from '@angular/core';
 
 
-const DATA = [
+const STOCK = [
   {
     name: 'Hungary',
-    value: 13419
+    value: 3
   },
   {
     name: 'Uzbekistan',
-    value: 21431
+    value: 21
   },
   {
     name: 'Côte D\'Ivoire',
-    value: 46654
+    value: 46
   },
   {
     name: 'Australia',
-    value: 35511
+    value: 70
   },
   {
     name: 'Brazil',
-    value: 19035
+    value: 19
   },
   {
     name: 'Spain',
-    value: 31582
+    value: 31
   },
   {
     name: 'Belgium',
-    value: 52194
+    value: 52
   },
   {
     name: 'American Samoa',
-    value: 10774
+    value: 107
   },
   {
     name: 'Christmas Island',
-    value: 52051
+    value: 52
   },
   {
     name: 'India',
-    value: 50089
+    value: 50
   },
   {
     name: 'Tuvalu',
-    value: 56507
+    value: 56
   },
   {
     name: 'Western Sahara',
-    value: 13674
+    value: 13
   },
   {
     name: 'Guernsey',
-    value: 27419
+    value: 27
   },
   {
     name: 'Gabon',
-    value: 59304
+    value: 59
   },
   {
     name: 'Solomon Islands',
-    value: 48069
+    value: 48
   },
   {
     name: 'Gambia',
-    value: 24229
+    value: 24
   },
   {
     name: 'Faroe Islands',
-    value: 51820
+    value: 51
   },
   {
     name: 'Lao People\'s Democratic Republic',
-    value: 11304
+    value: 11
   }
-]
+];
+
+const INGRESOS = [
+  {
+    name: 'ORDENES',
+    value: 34
+  },
+  {
+    name: 'GANANCIAS',
+    value: 20432
+  }
+];
+
+const MAS_VENDIDOS = [
+  {
+    name: 'Pizza',
+    series: [
+      {
+        value: 39,
+        name: 'Lunes'
+      },
+      {
+        value: 61,
+        name: 'Martes'
+      },
+      {
+        value: 66,
+        name: 'Miércoles'
+      },
+      {
+        value: 25,
+        name: 'Jueves'
+      },
+      {
+        value: 40,
+        name: 'Viernes'
+      },
+      {
+        value: 79,
+        name: 'Sábado'
+      }
+    ]
+  },
+  {
+    name: 'Hamburguesa',
+    series: [
+      {
+        value: 50,
+        name: 'Lunes'
+      },
+      {
+        value: 30,
+        name: 'Martes'
+      },
+      {
+        value: 50,
+        name: 'Miércoles'
+      },
+      {
+        value: 55,
+        name: 'Jueves'
+      },
+      {
+        value: 61,
+        name: 'Viernes'
+      },
+      {
+        value: 59,
+        name: 'Sábado'
+      }
+    ]
+  },
+  {
+    name: 'Pancho',
+    series: [
+      {
+        value: 65,
+        name: 'Lunes'
+      },
+      {
+        value: 64,
+        name: 'Martes'
+      },
+      {
+        value: 35,
+        name: 'Miércoles'
+      },
+      {
+        value: 35,
+        name: 'Jueves'
+      },
+      {
+        value: 21,
+        name: 'Viernes'
+      },
+      {
+        value: 58,
+        name: 'Sábado'
+      }
+    ]
+  },
+  {
+    name: 'Milanesa',
+    series: [
+      {
+        value: 64,
+        name: 'Lunes'
+      },
+      {
+        value: 42,
+        name: 'Martes'
+      },
+      {
+        value: 59,
+        name: 'Miércoles'
+      },
+      {
+        value: 22,
+        name: 'Jueves'
+      },
+      {
+        value: 65,
+        name: 'Viernes'
+      },
+      {
+        value: 73,
+        name: 'Sábado'
+      }
+    ]
+  },
+  {
+    name: 'Helado',
+    series: [
+      {
+        value: 44,
+        name: 'Lunes'
+      },
+      {
+        value: 36,
+        name: 'Martes'
+      },
+      {
+        value: 23,
+        name: 'Miércoles'
+      },
+      {
+        value: 48,
+        name: 'Jueves'
+      },
+      {
+        value: 38,
+        name: 'Viernes'
+      },
+      {
+        value: 74,
+        name: 'Sábado'
+      }
+    ]
+  }
+];
+
 
 @Component({
   selector: 'app-admin',
@@ -83,23 +243,45 @@ const DATA = [
 })
 export class AdminComponent implements OnInit {
 
-  single = DATA;
+  stock = STOCK;
+  ingresos = INGRESOS;
+  masVendidos = MAS_VENDIDOS;
 
-  // options
-  showXAxis = false;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
+  ingresosColorScheme = {
+    domain: ['#ffc107', '#ffa000']
+  };
 
-  colorScheme = 'fire';
+  masVendidosColorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onSelect(event) {
+  /* onSelect(event) {
     console.log(event);
+  } */
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
   }
 
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
+  format(c): string {
+    switch (c.label) {
+      case 'GANANCIAS':
+        return `\$${c.value.toLocaleString()}`;
+      default:
+        return c.value.toLocaleString();
+    }
+  }
 }
