712783592013731cda74dd6027e771bfd8ba3de9
Aaron Castro <aaroncrcr@gmail.com>
2020-06-19 15:43:35 -0300

    manufactured form

--- src/app/core/models/articulos/articulo-manufacturado.ts
+++ src/app/core/models/articulos/articulo-manufacturado.ts
@@ -6,6 +6,7 @@ export interface ArticuloManufacturado extends Base {
   denominacion: string;
   descripcion: string;
   imagen: string;
+  precio: number;
   tiempoEstimadoCocina: number;
   categoria: Categoria;
   detallesReceta: Array<DetalleReceta>;
--- src/app/pages/manufactured/manufactured-form/manufactured-form.component.ts
+++ src/app/pages/manufactured/manufactured-form/manufactured-form.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, Optional, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Categoria } from 'src/app/core/models/articulos/categoria';
+import { ArticuloManufacturado } from 'src/app/core/models/articulos/articulo-manufacturado';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-manufactured-form',
@@ -9,31 +11,74 @@ import { Categoria } from 'src/app/core/models/articulos/categoria';
 })
 export class ManufacturedFormComponent implements OnInit {
 
+  /* TEMPORAL */
   categorias: Categoria[] = [
-    { id: 1, oculto: false, eliminado: false, ultimaActualizacion: new Date(), denominacion: 'Pizzas', imagen: '🍕' },
-    { id: 2, oculto: false, eliminado: false, ultimaActualizacion: new Date(), denominacion: 'Burgers', imagen: '🍔' },
-    { id: 3, oculto: false, eliminado: false, ultimaActualizacion: new Date(), denominacion: 'Panchos', imagen: '🌭' },
-    { id: 4, oculto: false, eliminado: false, ultimaActualizacion: new Date(), denominacion: 'Carnes', imagen: '🍖' },
-    { id: 5, oculto: false, eliminado: false, ultimaActualizacion: new Date(), denominacion: 'Ensalada', imagen: '🥗' },
-    { id: 6, oculto: false, eliminado: false, ultimaActualizacion: new Date(), denominacion: 'Pastas', imagen: '🍝' },
-    { id: 7, oculto: false, eliminado: false, ultimaActualizacion: new Date(), denominacion: 'Asiatica', imagen: '🍣' },
-    { id: 8, oculto: false, eliminado: false, ultimaActualizacion: new Date(), denominacion: 'Postres', imagen: '🧁' },
-    { id: 9, oculto: false, eliminado: false, ultimaActualizacion: new Date(), denominacion: 'Desayuno', imagen: '☕' },
-    { id: 10, oculto: false, eliminado: false, ultimaActualizacion: new Date(), denominacion: 'Bebidas', imagen: '🥤' },
+    { id: 1, oculto: false, eliminado: false, ultimaActualizacion: null, denominacion: 'Pizzas', imagen: '🍕' },
+    { id: 2, oculto: false, eliminado: false, ultimaActualizacion: null, denominacion: 'Burgers', imagen: '🍔' },
+    { id: 3, oculto: false, eliminado: false, ultimaActualizacion: null, denominacion: 'Panchos', imagen: '🌭' },
+    { id: 4, oculto: false, eliminado: false, ultimaActualizacion: null, denominacion: 'Carnes', imagen: '🍖' },
+    { id: 5, oculto: false, eliminado: false, ultimaActualizacion: null, denominacion: 'Ensalada', imagen: '🥗' },
+    { id: 6, oculto: false, eliminado: false, ultimaActualizacion: null, denominacion: 'Pastas', imagen: '🍝' },
+    { id: 7, oculto: false, eliminado: false, ultimaActualizacion: null, denominacion: 'Asiatica', imagen: '🍣' },
+    { id: 8, oculto: false, eliminado: false, ultimaActualizacion: null, denominacion: 'Postres', imagen: '🧁' },
+    { id: 9, oculto: false, eliminado: false, ultimaActualizacion: null, denominacion: 'Desayuno', imagen: '☕' },
+    { id: 10, oculto: false, eliminado: false, ultimaActualizacion: null, denominacion: 'Bebidas', imagen: '🥤' },
   ];
 
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+  public localData: ArticuloManufacturado;
+  public action: string;
+  public manufacturedForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  get imagen(): FormControl {
+    return this.manufacturedForm.get('imagen') as FormControl;
+  }
 
-  ngOnInit() {
-    this.firstFormGroup = this.formBuilder.group({
-      firstCtrl: ['', Validators.required]
+  get tiempoEstimadoCocina(): FormControl {
+    return this.manufacturedForm.get('tiempoEstimadoCocina') as FormControl;
+  }
+
+  constructor(
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: ArticuloManufacturado,
+    public dialogRef: MatDialogRef<ManufacturedFormComponent>,
+    public formBuilder: FormBuilder
+  ) {
+    this.localData = { ...data };
+  }
+
+  ngOnInit(): void {
+    this.buildForm();
+    this.setAction();
+  }
+
+  buildForm() {
+    this.manufacturedForm = this.formBuilder.group({
+      id: [this.localData.id],
+      ultimaActualizacion: [this.localData.ultimaActualizacion],
+      oculto: [this.localData.oculto],
+      denominacion: [this.localData.denominacion, [Validators.required]],
+      descripcion: [this.localData.descripcion],
+      imagen: [this.localData.imagen, [Validators.required]],
+      precio: [this.localData.precio, [Validators.required]],
+      tiempoEstimadoCocina: [this.localData.tiempoEstimadoCocina ? this.localData.tiempoEstimadoCocina : 0, [Validators.required]],
+      categoria: [this.localData.categoria, [Validators.required]],
+      detallesReceta: [this.localData.detallesReceta, [Validators.required]],
     });
-    this.secondFormGroup = this.formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
+  }
+
+  setAction() {
+    this.action = (this.localData && (Object.keys(this.localData).length === 0)) ? 'Añadir' : 'Editar';
+  }
+
+  onAction() {
+    this.dialogRef.close({ event: this.action, data: this.manufacturedForm.value });
+  }
+
+  onCancel() {
+    this.dialogRef.close({ event: 'Cancel' });
+  }
+
+  errorHandling = (control: string, error: string) => {
+    return this.manufacturedForm.controls[control].hasError(error);
   }
 
   formatLabel(value: number) {
