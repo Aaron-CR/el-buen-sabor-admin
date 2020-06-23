4859326bdb0ff6f4f27de211f7b6600d249e626f
Aaron Castro <aaroncrcr@gmail.com>
2020-06-23 10:56:52 -0300

    order products tab updated

--- src/app/core/models/comprobantes/orden.ts
+++ src/app/core/models/comprobantes/orden.ts
@@ -2,14 +2,15 @@ import { Comprobante } from './comprobante';
 import { Cliente } from '../usuarios/cliente';
 import { DireccionDelivery } from '../direccion/direccion-delivery';
 import { Empleado } from '../usuarios/empleado';
-import { Factura } from './factura';
+import { DetalleOrden } from './detalle-orden';
 
 export interface Orden extends Comprobante {
+  aclaraciones: string;
+  detalles: Array<DetalleOrden>;
   delivery: boolean;
   tiempoTotalPreparacion: number;
   horarioEntrega: Date;
   cliente: Cliente;
   direccionEntrega: DireccionDelivery;
   repartidor: Empleado;
-  factura: Factura;
 }
--- src/app/shared/components/products-tab/notes/notes.component.ts
+++ src/app/shared/components/products-tab/notes/notes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-notes',
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
-  _notes: string;
-  // _notes = 'Extra ketchup';
+  @Input()
+  public data: string;
 
   get notes() {
-    return this._notes ? this._notes : 'Este pedido no tiene aclaraciones...';
+    return this.data ? this.data : 'Este pedido no tiene aclaraciones...';
   }
 
   constructor() { }
