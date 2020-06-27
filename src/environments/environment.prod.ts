9536121baa7f94b37013409c053a842e51c1640e
Aaron Castro <aaroncrcr@gmail.com>
2020-06-27 12:20:21 -0300

    firebase hosting config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDZHRmttCMVr-usvy0A8poGxRDgglHfMBU',
+    authDomain: 'elbuensabor-admin.firebaseapp.com',
+    databaseURL: 'https://elbuensabor-admin.firebaseio.com',
+    projectId: 'elbuensabor-admin',
+    storageBucket: 'elbuensabor-admin.appspot.com',
+    messagingSenderId: '984980672664',
+    appId: '1:984980672664:web:0388ade6fe1721a84300ed',
+    measurementId: 'G-3D6FX85VBR'
+  }
 };
