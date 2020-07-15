6c6812cbe200217893a8e79dd3cc99d81fbec6f4
florsalcedowd <florenciasalcedo13@gmail.com>
2020-07-15 17:46:25 -0300

    Update status working

--- src/app/shared/components/details-tab/status/status.component.ts
+++ src/app/shared/components/details-tab/status/status.component.ts
@@ -1,3 +1,4 @@
+import { OrderService } from './../../../services/order.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Estado } from 'src/app/core/models/comprobantes/estado';
 import { HttpClient } from '@angular/common/http';
@@ -15,17 +16,35 @@ interface Status {
 export class StatusComponent implements OnInit {
 
   @Input()
+  public orderId: number;
+  @Input()
   public data: Estado;
   @Input()
   public statuses: Array<Estado>;
 
-  constructor() { }
+  public selectedStatus: Estado;
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit(): void {
   }
 
   getStatusClass(status: string) {
     return status.replace(/\s+/g, '-');
+  }
+
+  changeStatus(newStatus){
+    this.selectedStatus = newStatus;
+    alert(this.selectedStatus.denominacion);
+  }
+
+  updateStatus(){
+    alert(this.selectedStatus.denominacion + ', ' + this.orderId);
+    this.orderService.updateEstado(this.selectedStatus, this.orderId).subscribe(
+      res => {
+        this.data = res.estado;
+      }
+    );
   }
 
 }
