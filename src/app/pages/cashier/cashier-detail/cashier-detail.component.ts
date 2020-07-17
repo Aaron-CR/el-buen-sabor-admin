1a96a7a1f521e797c8f742546b73d0040d5d8e40
Aaron Castro <aaroncrcr@gmail.com>
2020-07-17 12:54:10 -0300

    Minor improvement in status component

--- src/app/pages/cashier/cashier-detail/cashier-detail.component.ts
+++ src/app/pages/cashier/cashier-detail/cashier-detail.component.ts
@@ -1,10 +1,7 @@
-import { InvoiceService } from './../../../shared/services/invoice.service';
 import { Component, OnInit, Optional, Inject } from '@angular/core';
 import { Orden } from 'src/app/core/models/comprobantes/orden';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Estado } from 'src/app/core/models/comprobantes/estado';
-import { HttpClient } from '@angular/common/http';
-import { Factura } from 'src/app/core/models/comprobantes/factura';
 
 @Component({
   selector: 'app-cashier-detail',
@@ -18,43 +15,12 @@ export class CashierDetailComponent implements OnInit {
 
   constructor(
     @Optional() @Inject(MAT_DIALOG_DATA) public data: Orden,
-    public dialogRef: MatDialogRef<CashierDetailComponent>,
-    private http: HttpClient
+    public dialogRef: MatDialogRef<CashierDetailComponent>
   ) {
     this.localData = { ...data };
   }
 
   ngOnInit(): void {
-    this.getStatuses();
   }
-
-  getStatuses() {
-    return this.http.get(`http://localhost:8080/api/v1/comprobantes/estados/all`).pipe()
-      .subscribe((data: Array<Estado>) => {
-        switch (this.localData.estado.denominacion) {
-          case 'pendiente':
-            return this.statuses = data.filter(status => status.denominacion === 'cancelado' || status.denominacion === 'en proceso');
-          case 'cancelado':
-            return this.statuses = data.filter(status => status.denominacion === 'pendiente' || status.denominacion === 'en proceso');
-          case 'en proceso':
-            return this.statuses = data.filter(status => status.denominacion === 'demorado' || status.denominacion === 'listo');
-          case 'demorado':
-            return this.statuses = data.filter(status => status.denominacion === 'demorado' || status.denominacion === 'listo');
-          case 'listo':
-            return this.statuses = data.filter(status => status.denominacion === 'en camino' || status.denominacion === 'entregado');
-          case 'en camino':
-            return this.statuses = data.filter(status => status.denominacion === 'entregado');
-          case 'entregado':
-            return this.statuses = data.filter(status => status.denominacion === 'pagado');
-          case 'pagado':
-            return [];
-          case 'anulado':
-            return [];
-          default:
-            return this.statuses = data.filter(status => status.denominacion !== 'pagado' && status.denominacion !== 'anulado');
-        }
-      });
-  }
-
 
 }
--- src/app/shared/components/details-tab/status/status.component.ts
+++ src/app/shared/components/details-tab/status/status.component.ts
@@ -1,7 +1,8 @@
-import { OrderService } from './../../../services/order.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Estado } from 'src/app/core/models/comprobantes/estado';
 import { HttpClient } from '@angular/common/http';
+import { Orden } from 'src/app/core/models/comprobantes/orden';
+import { OrderService } from 'src/app/shared/services/order.service';
 
 interface Status {
   value: string;
@@ -16,33 +17,70 @@ interface Status {
 export class StatusComponent implements OnInit {
 
   @Input()
-  public orderId: number;
-  @Input()
-  public data: Estado;
+  public data: Orden;
   @Input()
   public statuses: Array<Estado>;
-
+  public allStatuses: Array<Estado>;
   public selectedStatus: Estado;
 
-  constructor(private orderService: OrderService) { }
+  constructor(
+    private orderService: OrderService,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    if (!this.statuses) {
+      this.getStatuses();
+    }
+  }
+
+  changeStatus(status: Estado) {
+    this.selectedStatus = status;
+  }
+
+  updateStatus() {
+    this.orderService.updateEstado(this.selectedStatus, this.data.id)
+      .subscribe(data => {
+        this.data = data;
+        this.filterStatuses(this.allStatuses);
+      });
+  }
+
+  getStatuses() {
+    return this.http.get(`http://localhost:8080/api/v1/comprobantes/estados/all`).pipe()
+      .subscribe((data: Array<Estado>) => {
+        this.allStatuses = data;
+        this.filterStatuses(this.allStatuses);
+      });
+  }
+
+  filterStatuses(data: Array<Estado>) {
+    switch (this.data.estado.denominacion) {
+      case 'pendiente':
+        return this.statuses = data.filter(status => status.denominacion === 'cancelado' || status.denominacion === 'en proceso');
+      case 'cancelado':
+        return this.statuses = data.filter(status => status.denominacion === 'pendiente' || status.denominacion === 'en proceso');
+      case 'en proceso':
+        return this.statuses = data.filter(status => status.denominacion === 'demorado' || status.denominacion === 'listo');
+      case 'demorado':
+        return this.statuses = data.filter(status => status.denominacion === 'demorado' || status.denominacion === 'listo');
+      case 'listo':
+        return this.statuses = data.filter(status => status.denominacion === 'en camino' || status.denominacion === 'entregado');
+      case 'en camino':
+        return this.statuses = data.filter(status => status.denominacion === 'entregado');
+      case 'entregado':
+        return this.statuses = data.filter(status => status.denominacion === 'pagado');
+      case 'pagado':
+        return [];
+      case 'anulado':
+        return [];
+      default:
+        return this.statuses = data.filter(status => status.denominacion !== 'pagado' && status.denominacion !== 'anulado');
+    }
   }
 
   getStatusClass(status: string) {
     return status.replace(/\s+/g, '-');
-  }
-
-  changeStatus(newStatus){
-    this.selectedStatus = newStatus;
-  }
-
-  updateStatus(){
-    this.orderService.updateEstado(this.selectedStatus, this.orderId).subscribe(
-      res => {
-        this.data = res.estado;
-      }
-    );
   }
 
 }
