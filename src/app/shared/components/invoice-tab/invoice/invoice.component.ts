f4d5dd0828a59547a7e2b0b05c1eb4541770e974
Aaron Castro <aaroncrcr@gmail.com>
2020-07-26 03:30:27 -0300

    send invoice via email

--- src/app/shared/components/invoice-tab/invoice/invoice.component.ts
+++ src/app/shared/components/invoice-tab/invoice/invoice.component.ts
@@ -6,6 +6,11 @@ import * as jspdf from 'jspdf';
 import html2canvas from 'html2canvas';
 import { MatDialogRef } from '@angular/material/dialog';
 import { CashierDetailComponent } from 'src/app/pages/cashier/cashier-detail/cashier-detail.component';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import emailjs from 'emailjs-com';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-invoice',
@@ -21,11 +26,16 @@ export class InvoiceComponent implements OnInit {
   public subtotal: number;
   public address: string;
   public invoiceColumns: string[] = ['producto', 'precio', 'cantidad', 'total'];
+  public uploadPercent: Observable<number>;
+  public urlImage: Observable<string>;
+  public isLoading = false;
 
   constructor(
     private dialogRef: MatDialogRef<CashierDetailComponent>,
     private invoiceService: InvoiceService,
-    private authService: AuthService
+    private authService: AuthService,
+    private storage: AngularFireStorage,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
@@ -88,6 +98,47 @@ export class InvoiceComponent implements OnInit {
       pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
       pdf.save(`invoice-${this.invoice.id}.pdf`);
     });
+  }
+
+  uploadInvoice() {
+    this.isLoading = true;
+    const data = document.getElementById('invoice');
+    html2canvas(data).then(canvas => {
+      const imgWidth = 208;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jspdf('p', 'mm', 'a4');
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      const file = pdf.output('blob');
+      const filePath = `invoices/invoice-${this.invoice.id}-${Date.now().toString()}.pdf`;
+      const ref = this.storage.ref(filePath);
+      const task = this.storage.upload(filePath, file);
+      this.uploadPercent = task.percentageChanges();
+      task.snapshotChanges().pipe(finalize(() => {
+        this.urlImage = ref.getDownloadURL();
+        this.urlImage.subscribe(url => {
+          this.sendEmail(url);
+        });
+      })).subscribe();
+    });
+  }
+
+  sendEmail(url) {
+    const templateParams = {
+      name: `${this.invoice.orden.cliente.nombre}`,
+      orderId: `${this.invoice.orden.id}`,
+      email: `${this.invoice.orden.cliente.email}`,
+      url: `${url}`,
+    };
+    emailjs.send('gmail', 'template_OgfPccBG', templateParams, 'user_p8QP1aFB0X1MMwAFmuUNu')
+      .then((response) => {
+        this.isLoading = false;
+        this.snackBar.open('Éxito! Se ha enviado el email', 'OK', { duration: 10000, panelClass: ['app-snackbar'] });
+      }, (err) => {
+        this.isLoading = false;
+        this.snackBar.open('Error! No pudimos enviar el email', 'OK', { duration: 10000, panelClass: ['app-snackbar'] });
+      });
   }
 
 }
